7efb1e64b1d9270bb4c4f15a99c0e0703dcca6a7
ndoyepato <issandmb19@gmail.com>
2019-11-07 16:52:01 +0000

    Page home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { Fontawesome } from 'nativescript-fontawesome';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Fontawesome} from 'nativescript-fontawesome';
+
 Fontawesome.init();
 
 @Component({
-  selector: 'ns-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'ns-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    selectedIndex0 = true;
+    selectedIndex1 = false;
+    selectedIndex2 = false;
+    selectedIndex3 = false;
+    @ViewChild("tab", {static: false}) tab: ElementRef;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onIndexChanged() {
+        console.log(this.tab.nativeElement.selectedIndex);
+        if (this.tab.nativeElement.selectedIndex == 0) {
+            this.selectedIndex0 = true;
+            this.selectedIndex1 = false;
+            this.selectedIndex2 = false;
+            this.selectedIndex3 = false;
+        } else if (this.tab.nativeElement.selectedIndex == 1) {
+            this.selectedIndex0 = false;
+            this.selectedIndex1 = true;
+            this.selectedIndex2 = false;
+            this.selectedIndex3 = false;
+        } else if (this.tab.nativeElement.selectedIndex == 2) {
+            this.selectedIndex0 = false;
+            this.selectedIndex1 = false;
+            this.selectedIndex2 = true;
+            this.selectedIndex3 = false;
+        } else if (this.tab.nativeElement.selectedIndex == 3) {
+            this.selectedIndex0 = false;
+            this.selectedIndex1 = false;
+            this.selectedIndex2 = false;
+            this.selectedIndex3 = true;
+        }
+    }
+
+    onMessage(){
+        console.log("Message icon clicked");
+    }
 
 }
